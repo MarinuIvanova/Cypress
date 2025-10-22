b42623e3bb7bb00b2dc6c0b20077ad4df68873d0
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-22 17:07:39 +0700

    change id token

--- cypress/e2e/spec_api.cy.js
+++ cypress/e2e/spec_api.cy.js
@@ -32,25 +32,26 @@ it.only("Login_API", () => {
     url: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/api/authenticate",
     body: { username: "marina_mg", password: "654321%", rememberMe: false },
   }).then((response) => {
-    const token = response.id_token
+    let token = response.body.id_token
     cy.log(token)
     expect(response.status).to.equal(200);
  
-  cy.request({
-      method: "POST",
-      headers: {
-        Authorization: "token",
-      },
-      url: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/api/account/change-password",
-      body: { currentPassword: "654321%", newPassword: "654321$" },
-    }).then((response) => {
-      expect(response.status).to.equal(200);
+      cy.request({
+          method: "POST",
+          
+          headers: {
+            Authorization: "Bearer" + token
+          },
+          url: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/api/account/change-password",
+          body: { currentPassword: "654321%", newPassword: "654321$" },
+        }).then((response) => {
+          expect(response.status).to.equal(200);
+        });
     });
-
     cy.visit("");
     cy.get("#account-menu").click();
     cy.get("#login-item").click();
     cy.login("marina_mg", "654321$");
     cy.get("#entity-menu").should("be.visible");
-   });
+   
 });
